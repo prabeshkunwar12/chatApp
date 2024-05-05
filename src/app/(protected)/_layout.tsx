e4bc760b8c0e434d2967c9e8c1dc4d646c6692ed
Prabesh Narsingh Kunwar <71028458+prabeshkunwar12@users.noreply.github.com>
2024-05-04 23:01:04 -0300

    stream chat provider

--- src/app/(protected)/_layout.tsx
+++ src/app/(protected)/_layout.tsx
@@ -1,42 +1,14 @@
-import { Slot, Stack } from "expo-router"
-import { useEffect } from "react";
-import { StreamChat } from 'stream-chat';
-import { Chat, OverlayProvider } from 'stream-chat-expo';
+import { Stack } from "expo-router"
 
-const client = StreamChat.getInstance('u56mgk2cp9yx');
+import ChatProvider from "../../providers/ChatProvider";
 
 const HomeLayout = () => {
-
-	useEffect(() => {
-		const connect = async () => {
-			await client.connectUser(
-				{
-					id: 'jlahey',
-					name: 'Jim Lahey',
-					image: 'https://i.imgur.com/fR9Jz14.png',
-				},
-				client.devToken('jlahey'),
-			);
-
-			// //once the channel is created, we don't need to create it again and again
-			// const channel = client.channel('messaging', 'the_park', {
-			// 	name: 'The Park',
-			// });
-			
-			// await channel.watch();
-		};
-
-		connect();
-	});
-
 	return (
-		<OverlayProvider>
-			<Chat client={client}>
-				<Stack>
-					<Stack.Screen name="(tabs)" options={{  headerShown: false }} />
-				</Stack>
-			</Chat>	
-		</OverlayProvider>
+		<ChatProvider>
+			<Stack>
+				<Stack.Screen name="(tabs)" options={{  headerShown: false }} />
+			</Stack>
+		</ChatProvider>
 	)
 }
 
