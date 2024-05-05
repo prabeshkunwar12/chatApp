5eb02d1493412ba39cefd53072db473bbe60906b
Prabesh Narsingh Kunwar <71028458+prabeshkunwar12@users.noreply.github.com>
2024-05-05 13:50:48 -0300

    profile picture

--- src/app/(protected)/(tabs)/profile.tsx
+++ src/app/(protected)/(tabs)/profile.tsx
@@ -1,8 +1,9 @@
 import { useState, useEffect } from 'react'
 import { supabase } from '../../../lib/supabase'
-import { StyleSheet, View, Alert } from 'react-native'
+import { StyleSheet, View, Alert, ScrollView } from 'react-native'
 import { Button, Input } from 'react-native-elements'
 import { useAuth } from '../../../providers/AuthProvider'
+import Avatar from '../../../components/Avatar'
 
 export default function ProfileScreen() {
     const { session } = useAuth();
@@ -84,7 +85,22 @@ export default function ProfileScreen() {
     }
 
     return (
-        <View style={styles.container}>
+        <ScrollView style={styles.container}>
+            <View style={{ alignItems: 'center' }}>
+                <Avatar 
+                    size={200} 
+                    url={avatarUrl} 
+                    onUpload={(url:string)=>{
+                        setAvatarUrl(url);
+                        updateProfile({ 
+                            username, 
+                            full_name:fullname, 
+                            website, 
+                            avatar_url:avatarUrl
+                        })
+                    }}                
+                />
+            </View>
             <View style={[styles.verticallySpaced, styles.mt20]}>
                 <Input label="Email" value={session?.user?.email} disabled />
             </View>
@@ -109,7 +125,7 @@ export default function ProfileScreen() {
             <View style={styles.verticallySpaced}>
                 <Button title="Sign Out" onPress={() => supabase.auth.signOut()} />
             </View>
-        </View>
+        </ScrollView>
     )
 }
 
